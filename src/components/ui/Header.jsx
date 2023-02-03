45130716014f66fc24f9714510166dba3d10ec63
James Dobbe <dobbe2@gmail.com>
2023-02-03 00:16:26 -0800

    init and start of new portfolio. create react app with tailwind css

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,238 @@
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartBarIcon,
+  CursorArrowRaysIcon,
+  DocumentChartBarIcon,
+  ShieldCheckIcon,
+  Squares2X2Icon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+const solutions = [
+  {
+    name: 'About',
+    description: 'Get a better understanding of where your traffic is coming from.',
+    href: '#',
+    icon: ChartBarIcon,
+  },
+  {
+    name: 'Experience',
+    description: 'Speak directly to your customers in a more meaningful way.',
+    href: '#',
+    icon: CursorArrowRaysIcon,
+  },
+  { name: 'Work', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
+  {
+    name: 'Contact',
+    description: "Connect with third-party tools that you're already using.",
+    href: '#',
+    icon: Squares2X2Icon,
+  },
+  {
+    name: 'Resume',
+    description: 'Build strategic funnels that will drive your customers to convert',
+    href: '#',
+    icon: ArrowPathIcon,
+  },
+]
+const resources = [
+  {
+    name: 'Help Center',
+    description: 'Get all of your questions answered in our forums or contact support.',
+    href: '#',
+  },
+  { name: 'Guides', description: 'Learn how to maximize our platform to get the most out of it.', href: '#' },
+  { name: 'Events', description: 'See what meet-ups and other events we might be planning near you.', href: '#' },
+  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  return (
+    <Popover className="relative bg-grey-100">
+      <div className="flex items-center justify-between p-6 md:justify-start md:space-x-10">
+        <div>
+          <a href="#" className="flex">
+            <span className="sr-only">Your Company</span>
+            <img
+              className="h-8 w-auto sm:h-10"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="-my-2 -mr-2 md:hidden">
+          <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <span className="sr-only">Open menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </Popover.Button>
+        </div>
+        <div className="hidden md:flex md:flex-1 md:items-center md:justify-end">
+          <Popover.Group as="nav" className="flex space-x-10">
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  {/* <Popover.Button
+                    className={classNames(
+                      open ? 'text-gray-900' : 'text-gray-500',
+                      'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                    )}
+                  > */}
+                  <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              About
+            </a>
+                    {/* <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-600' : 'text-gray-400',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button> */}
+
+                 
+                </>
+              )}
+            </Popover>
+            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              Experience
+            </a>
+            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              Work
+            </a>
+
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  {/* <Popover.Button
+                    className={classNames(
+                      open ? 'text-gray-900' : 'text-gray-500',
+                      'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                    )}
+                  > */}
+                  <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              Contact
+            </a>
+                    {/* <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-600' : 'text-gray-400',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button> */}
+
+                  
+                </>
+              )}
+            </Popover>
+          </Popover.Group>
+          <div className="flex items-center md:ml-12">
+            {/* <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              Sign in
+            </a> */}
+            <a
+              href="#"
+              className="ml-8 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+            >
+              Resume
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-6">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="mt-6">
+                <nav className="grid gap-6">
+                  {solutions.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 flex items-center rounded-lg p-3 hover:bg-gray-50"
+                    >
+                      <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-500 text-white">
+                        <item.icon className="h-6 w-6" aria-hidden="true" />
+                      </div>
+                      <div className="ml-4 text-base font-medium text-gray-900">{item.name}</div>
+                    </a>
+                  ))}
+                </nav>
+              </div>
+            </div>
+            {/* <div className="py-6 px-5">
+              <div className="grid grid-cols-2 gap-4">
+                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                  Pricing
+                </a>
+
+                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                  Docs
+                </a>
+
+                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                  Enterprise
+                </a>
+                {resources.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="mt-6">
+                <a
+                  href="#"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                >
+                  Sign up
+                </a>
+                <p className="mt-6 text-center text-base font-medium text-gray-500">
+                  Existing customer?{' '}
+                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                    Sign in
+                  </a>
+                </p>
+              </div>
+            </div> */}
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  )
+}
---
+++ src/components/ui/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function HeroComponent() {
+    return (
+        <div className="w-full p-20 bg-gray-100 h-96">
+            <div className="flex flex-col items-start justify-center">
+                <p className="text-lg text-violet-500">Hi, my name is</p>
+                <h1 className="text-4xl font-bold">James Dobbe</h1>
+                <h1 className="text-4xl">I am a front end developer.</h1>
+                <p className="text-md">I design and build custom websites for cool people, just like you! I am currently working as a freelancer, designing brands and sites for the most exciting groups.</p> 
+                <div className="mt-4">
+                    <button className="px-6 py-2 text-center text-white bg-indigo-600 rounded-md shadow-md">
+                        Get started
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
