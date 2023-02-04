9a394bb313a6c4153cf1a13f0a4c020054c7c0ad
James Dobbe <dobbe2@gmail.com>
2023-02-04 11:59:11 -0800

    updating styling and spacing for hero

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HeroComponent() {
     return (
-        <div className="w-full p-20 bg-gray-100 h-96">
+        <div className="w-full py-16 px-6 md:px-24 md:p-24  bg-gray-100 lg:h-96 h-auto">
             <div className="flex flex-col items-start justify-center">
                 <p className="text-lg text-violet-500">Hi, my name is</p>
                 <h1 className="text-4xl font-bold">James Dobbe</h1>
