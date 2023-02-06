e9678fc2bde959fd235fade423f410c04afd198a
James Dobbe <dobbe2@gmail.com>
2023-02-06 15:48:18 -0800

    adding custom color pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'purple-heart': '#5f22d9',
+      'viking': '#51d6ca',
+      'dusty-gray': '#9c9c9c',
+      'mercury': '#e4e4e4',
+      'galliano': '#d7ad0d',
+      'christale': '#2c016d',
+      'scorpion': '#5b5b5b',
+      'heliotrope': "#ac73ff",
+      'persian-red': '#c82b28',
+    },
     extend: {},
   },
   plugins: [
