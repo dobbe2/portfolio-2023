00ba9ea1b79700a61cce6866c972622320291f47
James Dobbe <dobbe2@gmail.com>
2023-02-16 23:31:14 -0800

    created button component with props for label and font size

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//for font-size, use Tailwind text-sm text#xl etc..
+export default function Button ({ size, label }) {
+    return (
+        <button className={`px-6 py-2 text-center ${size} text-viking font-semibold border-2 border-viking rounded-md shadow-md hover:bg-viking-tint transition hover:ease-in-out`}>
+            { label }
+        </button>
+    )
+}
