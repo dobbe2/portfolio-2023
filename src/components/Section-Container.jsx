b17fc8ffbfc450d29caf12f8faf4c8dac487a838
James Dobbe <dobbe2@gmail.com>
2023-03-10 00:15:43 -0800

    Padding and Margin for each UI section

---
+++ src/components/Section-Container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SectionContainer({ children }) {
+    return (
+    <div className="overflow-hidden bg-white">
+        <div className="relative w-full py-16 px-6 md:px-24 md:p-24 ">
+            {children}
+        </div>
+    </div>
+    )
+}
