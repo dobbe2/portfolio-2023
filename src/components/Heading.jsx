b81ef8706fd67162053e8022feec5d87ddca99f3
James Dobbe <dobbe2@gmail.com>
2023-03-10 00:10:40 -0800

    Reusable Heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Heading({ title }) {
+    return (
+        <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-indigo-600 sm:text-4xl">
+            {title}
+        </h3>
+    )
+}
