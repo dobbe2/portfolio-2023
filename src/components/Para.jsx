4ebf965d184354ccc438ed38688477f02274dad7
James Dobbe <dobbe2@gmail.com>
2023-03-28 00:35:09 -0700

    Added classname props to Para

--- src/components/Para.jsx
+++ src/components/Para.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Para({ bold, children }) {
-  return <p className={bold ? "text-lg text-gray-500 font-medium" : "text-lg text-gray-500"}>{children}</p>;
+export default function Para({ bold, className, children }) {
+  return <p className={bold ? `text-lg text-gray-500 font-bold ${className}` : `text-lg text-gray-500 ${className}`}>{children}</p>;
 }
