c7b8e4e9f3b548cc333cf1384312e3905a970042
James Dobbe <dobbe2@gmail.com>
2023-03-28 00:05:44 -0700

    Added bold turnary to paragraph, updated About section

--- src/components/ui/About.jsx
+++ src/components/ui/About.jsx
@@ -6,14 +6,14 @@ import SectionContainer from "../Section-Container";
 export default function About() {
   return (
     <SectionContainer>
-      <div className="absolute top-0 bottom-0 left-3/4 hidden w-screen bg-gray-50 lg:block" />
+      <div className="absolute top-0 bottom-0 hidden w-screen left-3/4 bg-gray-50 lg:block" />
       <div className="mx-auto text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
         <Heading title="About Me" />
       </div>
       <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
         <div className="relative lg:col-start-2 lg:row-start-1">
           <svg
-            className="absolute top-0 right-0 -mt-20 -mr-20 hidden lg:block"
+            className="absolute top-0 right-0 hidden -mt-20 -mr-20 lg:block"
             width={404}
             height={384}
             fill="none"
@@ -50,16 +50,16 @@ export default function About() {
               <div class="filter grayscale-[50%] hover:filter-none hover:contrast-200 hover:duration-150 duration-150 ease-in-out">
                 <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
                   <img
-                    className="rounded-lg object-cover object-center shadow-lg"
+                    className="object-cover object-center rounded-lg shadow-lg lg:max-w-md"
                     src={Me}
-                    alt="Whitney leaning against a railing on a downtown street"
+                    alt="My daughter and I at a restaurant"
                     width={1184}
                     height={1376}
                   />
                 </div>
               </div>
-              <figcaption className="mt-3 flex text-sm text-gray-500">
-                {/* <CameraIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" /> */}
+              <figcaption className="flex mt-3 text-sm text-gray-500">
+                {/* <CameraIcon className="flex-none w-5 h-5 text-gray-400" aria-hidden="true" /> */}
                 <span className="ml-2">
                   My daughter and I having too much fun
                 </span>
@@ -77,7 +77,7 @@ export default function About() {
               engaging pages and components.{" "}
             </Para>
           </div>
-          <div className="prose prose-indigo mt-5 text-gray-500 lg:col-start-1 lg:row-start-1 lg:max-w-none">
+          <div className="mt-5 prose text-gray-500 prose-indigo lg:col-start-1 lg:row-start-1 lg:max-w-none">
             <Para>
               I've worked for a large crypto firm, helping to build and
               implement a complete redesign of a handful of applications for
@@ -87,6 +87,9 @@ export default function About() {
             <Para>
               I have also worked with businesses to build modern websites that
               are designed to help drive more traffic and engage their users.
+            </Para>
+            <Para>
+              Here are a few technologies I’ve been working with recently:
             </Para>
             <div class="grid grid-cols-2 gap-4">
               <ul>
