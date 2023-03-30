c06fd8d495ffb0928fcf641fa912ccb7fc24808e
James Dobbe <dobbe2@gmail.com>
2023-03-30 15:16:02 -0700

    joining of classnames

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-export default function Heading({ title }) {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Heading({ title, dark }) {
   return (
-    <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-indigo-600 sm:text-4xl">
+    <h3 className={classNames(dark ? 'text-viking' : 'text-indigo-600' , 'mt-2 text-3xl font-bold leading-8 tracking-tight sm:text-4xl')}>
       {title}
     </h3>
   );
