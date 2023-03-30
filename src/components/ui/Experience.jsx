6ebcb0698a7aaea489075482ae7494394a6499ed
James Dobbe <dobbe2@gmail.com>
2023-03-30 15:17:15 -0700

    updated components and props, updated look of exp section

--- src/components/ui/Experience.jsx
+++ src/components/ui/Experience.jsx
@@ -2,29 +2,31 @@ import { PaperClipIcon } from '@heroicons/react/20/solid'
 import Heading from '../Heading'
 import SectionContainer from '../Section-Container'
 import Para from '../Para'
+import Title from '../Title'
+import ListItem from '../ListItem'
 
 export default function Experience() {
   return (
-    <SectionContainer>
-        <Heading title="Where I've Worked" />
+    <SectionContainer className="bg-purple-heart">
+        <Heading dark title="Where I've Worked" />
       <div className="mt-5 prose text-gray-500 prose-indigo lg:col-start-1 lg:row-start-1 lg:max-w-none">
         <dl className="grid grid-cols-1 lg:mt-10 gap-x-4 gap-y-0 lg:gap-y-8 sm:grid-cols-2">
           <div className="sm:col-span-1">
-            <Para bold>Title</Para>
-            <Para>Freelance Web Developer</Para>
+            <Title dark >Position</Title>
+            <Para dark>Freelance Web Developer</Para>
           </div>
           <div className="sm:col-span-1">
-            <Para bold>Date</Para>
-            <Para>February 2021 - Present</Para>
+            <Title dark>Date</Title>
+            <Para dark>February 2021 - Present</Para>
           </div>
           <div className="sm:col-span-2">
-          <Para bold>What I've Done</Para>
+          <Title dark>What I've Done</Title>
             <div className="grid lg:gap-4">
-              <ul className='list-disc'>
-                <li>Work with businesses to design and develop websites and branding/logos</li>
-                <li>Assist in developing creative content</li>
-                <li>Work with client to ensure shared vision and expectations</li>
-                <li>Launched multiple websites, setting up domains, hosting, and monitoring/maintenance</li>
+              <ul role="list" className='text-lg list-disc marker:text-viking'>
+                <ListItem dark item="Work with businesses to design and develop websites and branding/logos" />
+                <ListItem dark item="Assist in developing creative content" />
+                <ListItem dark item="Work with client to ensure shared vision and expectations" />
+                <ListItem dark item="Launched multiple websites, setting up domains, hosting, and monitoring/maintenance" />
               </ul>
             </div>
           </div>
@@ -33,21 +35,21 @@ export default function Experience() {
         <div className="mt-5 prose text-gray-500 prose-indigo lg:col-start-1 lg:row-start-1 lg:max-w-none">
         <dl className="grid grid-cols-1 lg:mt-10 gap-x-4 gap-y-8 sm:grid-cols-2">
           <div className="sm:col-span-1">
-            <Para bold>Title</Para>
-            <Para>Front End Developer @ ZAP Protocol</Para>
+            <Title dark>Position</Title>
+            <Para dark>Front End Developer @ ZAP Protocol</Para>
           </div>
           <div className="sm:col-span-1">
-            <Para bold>Date</Para>
-            <Para>October 2021 - April 2022</Para>
+            <Title dark>Date</Title>
+            <Para dark>October 2021 - April 2022</Para>
           </div>
           <div className="sm:col-span-2">
-            <Para bold>What I've Done</Para>
+            <Title dark>What I've Done</Title>
             <div className="grid lg:gap-4">
-              <ul className='list-disc'>
-                <li>Used design tools such as Figma and Storybook to create reusable components for the entire application and to build and test all components</li>
-                <li>Worked alongside design team through handoffs and bridges. Collaborated daily with front-end and design teams to keep progress pushing forward and give and receive feedback on current tasks</li>
-                <li>Received and reviewed changes to the Design System and created tickets to assign to developers</li>
-                <li>Performed bug testing and QA before version releases and made changes quickly and precisely</li>
+            <ul role="list" className='text-lg list-disc marker:text-viking'>
+                <ListItem dark item="Used design tools such as Figma and Storybook to create reusable components for the entire application and to build and test all components" />
+                <ListItem dark item="Worked alongside design team through handoffs and bridges. Collaborated daily with front-end and design teams to keep progress pushing forward and give and receive feedback on current tasks" />
+                <ListItem dark item="Received and reviewed changes to the Design System and created tickets to assign to developers" />
+                <ListItem dark item="Performed bug testing and QA before version releases and made changes quickly and precisely" />
               </ul>
             </div>
           </div>
