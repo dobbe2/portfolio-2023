65f76e63dd2e62995bf5e685862baa0e1dfafb8e
James Dobbe <dobbe2@gmail.com>
2023-04-11 00:49:51 -0700

    adding href to projects, updated XL grid spacing

--- src/components/ui/Creations.jsx
+++ src/components/ui/Creations.jsx
@@ -1,5 +1,6 @@
 import Heading from "../Heading";
 import Para from "../Para";
+import Title from "../Title";
 import SectionContainer from "../Section-Container";
 import anagram from "../../assets/images/anagram.png";
 import baseball from "../../assets/images/baseballApp.png";
@@ -11,34 +12,39 @@ import walkeasy from "../../assets/images/walkEasy1.png";
 const features = [
     {
       name: 'Pedorthist Medical Website',
-      description: 'Designed and created a company site for a Medical Professional (https://walk-easy.vercel.app) to replace a very slow and poorly designed WordPress site.',
+      description: `Designed and created a company site for a Medical Professional to replace a very slow and poorly designed WordPress site.`,
       imageSrc: walkeasy,
       imageAlt: 'Hero Section of the WalkEasy foot doctor website',
-      tools: ['Next.js', 'Tailwind CSS', 'Tailwind UI'],
+      tools: `Next.js Tailwind CSS Tailwind UI`,
+      href: 'http://walk-easy.vercel.app',
     },
     {
       name: 'Construction Contractor Website',
-      description: 'Built a WordPress website for a construction company (https://leebrannamconstruction.com) to showcase projects and drive business with an elegant and modern responsive design. ',
+      description: 'Built a WordPress website for a construction company to showcase projects and drive business with an elegant and modern responsive design. ',
       imageSrc: lbc,
       imageAlt: 'Detail of zipper pull with tan leather and silver rivet.',
+      href: 'https://leebrannamconstruction.com'
     },
     {
         name: 'ZAP NFT Marketplace',
         description: 'Remotely worked with developers and designers from around the world, contributing to the redesign of a large application containing an NFT marketplace, Oracles Price Feeds, and other Exchanges. Designed with mobile development as top priority.  Project can be found at https://app.zap.org/nft (requires digital wallet to view all content). ',
         imageSrc: nft,
         imageAlt: 'Frontpage layout of the ZAP Protocol NFT Marketplace',
+        href: 'https://app.zap.org/nft'
     },
     {
         name: 'Interactive Baseball Scoreboard',
         description: 'Developed a visual representation of a baseball scoreboard.  Programmed the many conditional situations that occur during a ball and strike count, and the runners on the basepaths. Focused on heavy game logic.',
         imageSrc: baseball,
         imageAlt: 'Picture of the baseball game app with a game in progress.',
+        href: 'https://github.com/dobbe2/baseballScoreboard'
     },
     {
         name: 'Anagram Checker',
         description: 'A project created to check if two sets of letters or words are anagrams of each other.',
         imageSrc: anagram,
         imageAlt: 'Picture of the anagram app.',
+        href: 'https://dobbe2.github.io/anagram',
     },
   ]
   
@@ -63,21 +69,26 @@ const features = [
                     'mt-6 lg:col-span-7 lg:row-start-1 lg:mt-0 xl:col-span-6'
                   )}
                 >
-                  <Para bold className="hover:text-viking">{feature.name}</Para>
+                  <Para bold>Featured Project </Para>
+                  <a target="_blank" rel="noreferrer" href={feature.href}>
+                    <Title bold className="transition duration-300 hover:text-viking">{feature.name}</Title>
+                  </a>
                   <Para className="mt-2">{feature.description}</Para>
                   <Para bold className="mt-2">Technologies Used</Para>
                     <ul>
-                        {features.tools}
+                        {feature.tools}
                     </ul>
                 </div>
                 <div
                   className={classNames(
-                    featureIdx % 2 === 0 ? 'lg:col-start-8 xl:col-start-7' : 'lg:col-start-1',
-                    'flex-auto lg:col-span-5 lg:row-start-1 xl:col-span-6'
+                    featureIdx % 2 === 0 ? 'lg:col-start-8 ' : 'lg:col-start-1',
+                    'flex-auto lg:col-span-5 lg:row-start-1 '
                   )}
                 >
                   <div className="overflow-hidden bg-gray-100 rounded-lg shadow-lg aspect-w-5 aspect-h-5">
-                    <img src={feature.imageSrc} alt={feature.imageAlt} className="object-cover object-center" />
+                    <a target="_blank" rel="noreferrer" href={feature.href}>
+                      <img src={feature.imageSrc} alt={feature.imageAlt} className="object-cover object-center" />
+                    </a>
                   </div>
                 </div>
               </div>
