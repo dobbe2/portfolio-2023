50a4c18f5907ed45edf0742fd9c3682997efb214
James Dobbe <dobbe2@gmail.com>
2023-04-19 21:37:55 -0700

    added classnames option for button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 //for font-size, use Tailwind text-sm text#xl etc..
-export default function Button({ size, label }) {
+export default function Button({ size, label, className }) {
   return (
     <button
-      className={`px-6 py-2 text-center ${size} text-viking font-semibold border-2 border-viking rounded-md shadow-md hover:bg-viking-tint transition hover:ease-in-out`}
+      className={classNames( `${className} px-6 py-2 text-center ${size} text-viking font-semibold border-2 border-viking rounded-md shadow-md hover:bg-viking-tint transition hover:ease-in-out`)}
     >
       {label}
     </button>
