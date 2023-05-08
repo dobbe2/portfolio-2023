50803a7a0d93ec45a4c5c390b6efbe103ab55dcd
James Dobbe <dobbe2@gmail.com>
2023-05-08 00:39:33 -0700

    Built the two shine divs required for custom animation

---
+++ src/components/Shine.jsx
@@ -0,0 +1,5 @@
+export default function Shine() {
+  return (
+    <div className="absolute top-0 z-0 block w-1/4 h-full transform -skew-x-12 -inset-y-full -right-full -left-1/3 bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine" />
+  );
+}
---
+++ src/components/ShineOuter.jsx
@@ -0,0 +1,7 @@
+export default function ShineOuter({ children }) {
+  return (
+            <div className="relative flex cursor-pointer group">
+              {children}
+            </div>
+  );
+}
